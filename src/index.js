a515edb4bdacecfae37960c9ca79ec6c94a509d8
Arild Grimstveit <arild@grimstveit.no>
2022-06-13 17:09:29 +0200

    Update status bar to reflect next turn

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X";
+    const status = "Next turn: " + (this.state.xIsNext ? "X" : "O");
 
     return (
       <div>
